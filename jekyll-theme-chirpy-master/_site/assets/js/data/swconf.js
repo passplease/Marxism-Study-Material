1a795417104c94dc9d2e3ec67da3aea96f7c8f84
passplease <1175851421@qq.com>
2025-01-23 17:34:32 +0800

    fix bugs with deploy

--- jekyll-theme-chirpy-master/_site/assets/js/data/swconf.js
+++ jekyll-theme-chirpy-master/_site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737620627',resources: [
+    cacheName: 'chirpy-1737624782',resources: [
       '/Marxism-Study-Material/assets/css/jekyll-theme-chirpy.css',
       '/Marxism-Study-Material/',
       

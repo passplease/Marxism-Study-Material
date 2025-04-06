a98926e9f02c28d59a5d61f3044dba0b3d1a97f7
passplease <1175851421@qq.com>
2025-04-06 19:06:44 +0800

    Research Poland

--- jekyll-theme-chirpy-master/_site/assets/js/data/swconf.js
+++ jekyll-theme-chirpy-master/_site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738087338',resources: [
+    cacheName: 'chirpy-1743937171',resources: [
       '/Marxism-Study-Material/assets/css/jekyll-theme-chirpy.css',
       '/Marxism-Study-Material/',
       
